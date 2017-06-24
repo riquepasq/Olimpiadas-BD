278c8a4b9543026f6a0ed93c4b301acf0cb68372
Henrique <riquepasq@hotmail.com>
2017-06-23 21:27:59 -0300

    Initial DB

---
+++ src/database/Conexao.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Conexao {
+    private Connection conn; 
+    private String username = "h8532252";
+    private String password = "8532252";
+    public Conexao(){
+        String driver = "thin";
+        String hostname = "grad.icmc.usp.br";
+        String port = "15215";
+        String sid = "orcl";
+        String strcon = "jdbc:oracle:"+driver+":@"+hostname+":"+port+":"+sid;
+        try{
+            this.conn = DriverManager.getConnection(strcon, username, password);             
+            System.out.println("Conectado!");
+        }catch(SQLException ex){
+            System.out.println("Erro de Conexao");
+            ex.printStackTrace();
+        }
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+    
+}
---
+++ src/database/getConnection.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.*;
+
+/**
+ *
+ * @author lenovo
+ */
+public class getConnection {
+    private Connection conn;
+    private Statement st;
+    private ResultSet rs;   
+    
+    public void closeConnection(){
+        try{
+            st.close();
+            rs.close();
+            conn.close();
+        }catch(SQLException ex){
+            System.out.println("Erro ao fechar Conexao");
+        }
+        
+    }
+    public void openConnection(/*String servidor, String porta, String banco, String user, String pwd*/){
+        String driver = "thin";
+        String hostname = "grad.icmc.usp.br";
+        String port = "15215";
+        String sid = "orcl";
+        String username = "e9778536";
+        String password = "9085583";
+        String strcon = "jdbc:oracle:"+driver+":@"+hostname+":"+port+":"+sid;
+        try{
+            this.conn = DriverManager.getConnection(strcon, username, password); 
+            //Statement para a consulta
+            this.st = conn.createStatement();
+            System.out.println("Conectado!");
+        }catch(SQLException ex){
+            System.out.println("Erro de Conexao");
+            ex.printStackTrace();
+        }
+        
+    }
+    public void executarQuery(String query) throws SQLException{
+        this.rs = st.executeQuery(query);
+        //Varre tupla a tupla
+        while (rs.next()){
+            System.out.println(rs.getString("POSICAO")+" "+rs.getString("JOGADOR"));
+        }
+    }
+    public void executarUpdate(String query) throws SQLException{
+        st.executeUpdate(query);
+    
+    }
+    public void exibeConsulta() throws SQLException{
+        StringBuffer str = new StringBuffer();
+        int colCount = rs.getMetaData().getColumnCount();
+        while(rs.next()){
+            for(int i = 0;i <= colCount; i++){
+                str.append(rs.getString(i) + "\t");
+            }
+            str.append("\n");
+        }
+        System.out.println(str);
+    }
+
+}
